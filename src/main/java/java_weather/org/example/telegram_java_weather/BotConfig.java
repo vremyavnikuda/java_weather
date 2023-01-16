2918bcd5bc33e0896999cb5f287234b31d9dd073
vremayvnikuda <nevsky.andrew@gmail.com>
2023-01-16 19:07:26 +0700

    добавлен класс конфигурации бота

---
+++ src/main/java/java_weather/org/example/telegram_java_weather/BotConfig.java
@@ -0,0 +1,38 @@
+package java_weather.org.example.telegram_java_weather;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "bot_config")
+
+
+public class BotConfig {
+    @Id
+    private BigInteger id;
+
+    //имя бота
+    private String name;
+
+    //токер telegram
+    private String accessToken;
+
+    //шаблон API ссылки (получать данный о текущей погоде)
+    //http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric&lang=ru
+    private String nowWeatherAPIToken;
+
+    //кнопки перенаправления пользователя на сторонние ресурсы
+    //https://api.telegram.org/bot{token}/answerCallbackQuery?callback_query_id={id}
+    private String telegramCallbackAnswerTemp;
+
+    //место, где будут храниться наши команды бота
+    private List<Command>commands;
+}
