4038a1d6b8dee4d52b713fbda726d1be195c6ccd
vremayvnikuda <nevsky.andrew@gmail.com>
2023-01-17 19:10:33 +0700

    upd cd

--- src/main/java/java_weather/org/example/telegram_java_weather/inter/BotConfigRepo.java
+++ src/main/java/java_weather/org/example/telegram_java_weather/inter/BotConfigRepo.java
@@ -4,7 +4,7 @@ import java_weather.org.example.telegram_java_weather.BotConfig;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.math.BigInteger;
-
+//пустой интерфейс
 public interface BotConfigRepo extends MongoRepository<BotConfig, BigInteger> {
 
 }
